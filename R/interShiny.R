ebf468cbe298bd6f5aa1b640e0bfd246a826c05b
stratos moschidis <smos@uom.edu.gr>
2022-12-28 21:45:03 +0200

    asd

--- R/interShiny.R
+++ R/interShiny.R
@@ -1,11 +1,11 @@
 
 interShiny<-function(){
   # Get the directory from the path of the current file.
- library(this.path)
-  cur_dir2 = dirname(this.path())
+ #library(this.path)
+  #cur_dir2 = dirname(this.path())
 
   # Set the working directory.
- setwd(cur_dir2)
+ #setwd(cur_dir2)
   source('app/server.R')
   shiny::runApp('R/app')
 
