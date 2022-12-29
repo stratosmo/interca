8caf9e93a3f17091dd4cc512ca18254ae520cd85
stratos moschidis <smos@uom.edu.gr>
2022-12-29 16:34:16 +0200

    commited

--- R/interShiny.R
+++ R/interShiny.R
@@ -7,7 +7,7 @@ interShiny<-function(){
   # Set the working directory.
  #setwd(cur_dir2)
   #source('app/server.R')
-  shiny::runApp("R")
+  shiny::runApp()
 #shinyApp(ui = system.file("R", "ui.R", package = "test"),
        #  server = system.file("R", "server.R", package = "test"))
 }
