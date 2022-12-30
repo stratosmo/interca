81ccd64cbb3bc83d7d39d297e5669f9147f29c77
stratos moschidis <smos@uom.edu.gr>
2022-12-30 19:17:20 +0200

    added documentation for functions

--- R/interShiny.R
+++ R/interShiny.R
@@ -14,9 +14,9 @@
 #shiny::runApp("R")
 
   interShiny= function(...) {
-    appDir = system.file("interShiny", package = "test")
+    appDir = system.file("interShiny", package = "interCa")
     if (appDir == "") {
-      stop("Could not find example directory. Try re-installing `shinyMlr`.", call. = FALSE)
+      stop("Could not find example directory. Try re-installing `interCa`.", call. = FALSE)
     }
 
     shiny::runApp(appDir, display.mode = "normal")
