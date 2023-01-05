ba8413aa08949bec4d9dd3c737756dca7da0f09e
stratos moschidis <smos@uom.edu.gr>
2023-01-05 13:22:14 +0200

    Update ui.R

--- inst/interShiny/ui.R
+++ inst/interShiny/ui.R
@@ -83,8 +83,8 @@ shinyUI(
                      ),
             tabPanel("Interpretive plane",
                      fluidRow(
-                       column(6,  numericInput("which.xaxis","Select which axis will be the x-axis",value=NA),
-                              numericInput("which.yaxis","Select which axis will be the y-axis",value=NA),
+                       column(6,  numericInput("which.xaxis","Select which axis will be the x-axis",value=1),
+                              numericInput("which.yaxis","Select which axis will be the y-axis",value=2),
                               checkboxInput("cb_slider_plane","Manual filtering",value = T),
                               sliderInput("slider_plane","Filter points",value = 50,min=0,max=100),
                               actionButton("do_plane","Create plane and table",class="btn-success"),
